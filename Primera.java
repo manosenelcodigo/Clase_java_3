908eb1ee0f40aadb3b507da5c0e17d9bd0ea4213
#manosenelcódigo <peligrosamente@gmail.com>
2016-05-05 23:21:21 -0300

    Primera.java

---
+++ Primera.java
@@ -0,0 +1,37 @@
+package com.manosenelcodigo.clase;
+//modificador de acceso
+//declarar una clase
+public class Primera 
+{
+    //atributos o propiedades
+    //tipos de dato primitivos
+    private int numero,numero2;
+    private boolean validador;
+    private float decimal;
+    private double otrodecimal;
+    //tipos de datos objeto
+    private String texto;
+    
+    
+    //uno o varios(sobrecarga) constructor
+    Primera()
+    {
+        this.numero=45;
+    }
+    
+    
+    //métodos 
+    //de ejecución o de retorno
+    //de tipo estáticos 
+    public void muestraInformacion()
+    {
+        
+    }
+    public String otroMetodo()
+    {
+        return null;
+    }
+    
+    //clases internas
+    
+}
